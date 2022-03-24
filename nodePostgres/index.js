ea2d0d96d0b32c38a3e271b75155823c2fc52dde
Vladislav342 <braunvlad981@gmail.com>
2022-03-24 22:55:13 +0300

    PostgreError

---
+++ nodePostgres/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const userRouter = require('./routes/user.routes')
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/api', userRouter)
+
+app.listen(PORT, () => console.log('server is started'));
