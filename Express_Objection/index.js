e88e6100cb91add62e396a315899aef6c36e919b
Vladislav342 <braunvlad981@gmail.com>
2022-04-06 22:16:34 +0300

    Express + Objection + Knex

--- Express_Objection/index.js
+++ Express_Objection/index.js
@@ -1,15 +1,30 @@
 const dbSetup = require('./db/db-setup');
 const express = require('express');
-const User = require('../db/models/user');
+const User = require('./db/models/user');
 
 dbSetup();
 
 const app = express();
 app.use(express.json());
 
-app.get('/user/:id', (req, res, next) => {
-	const {id} = req.params;
-	const user = await User.query().findBy(id);
+//in prod put this in separate files to avoid code smells
+//this is just for the tutorial 
+app.get('/user', async (req,res,next) => {
+	const user = await User.query().withGraphFetched('channel');
+	res.json(user);
 })
+
+app.get('/user/:id', async (req, res, next) => {
+	try{
+		const {id} = req.params;
+		const user = await User.query().findById(id).withGraphFetched('channel');
+		res.json(user);
+	}catch(err){
+		console.log(err);
+		res.status(500).json(err);
+	}
+})
+
+
 
 app.listen(8080, () => console.log('server running on port 8080'));
